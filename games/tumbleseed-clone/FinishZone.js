08a9da6b915f9a7719206e101875f7fd9a6c7608
Samuel Mosley <samueljmosley@gmail.com>
2017-12-13 15:10:47 -0600

    convert finishzone and hole to ES6 classes

--- games/tumbleseed-clone/FinishZone.js
+++ games/tumbleseed-clone/FinishZone.js
@@ -1,10 +1,12 @@
-function FinishZone(){
-    this.x = width/3;
-    this.y = 0;
-    this.w = width/3;
-    this.h = 50;
+class FinishZone{
+    constructor(){
+        this.x = width/3;
+        this.y = 0;
+        this.w = width/3;
+        this.h = 50;
+    }
 
-    this.display = function(){
+    display(){
         fill(255, 253, 84);
         rect(this.x, this.y, this.w, this.h);
     }
--- games/tumbleseed-clone/Hole.js
+++ games/tumbleseed-clone/Hole.js
@@ -1,9 +1,11 @@
-function Hole(_x, _y, _r){
-    this.x = _x;
-    this.y = _y;
-    this.r = _r
+class Hole{
+    constructor(_x, _y, _r){
+        this.x = _x;
+        this.y = _y;
+        this.r = _r;
+    }
 
-    this.display = function(){
+    display(){
         ellipseMode(RADIUS);
         fill(47, 106, 22)
         ellipse(this.x, this.y-4, this.r, this.r);
