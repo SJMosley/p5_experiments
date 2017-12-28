de8bf193712e628f4ca0dd2bb71beb924c946f02
Samuel Mosley <samueljmosley@gmail.com>
2017-12-28 14:12:36 -0600

    rename random experiments folder, add 2d perlin for useage later before building the flow field

--- coding-challenges/20-julia-set/sketch.js
+++ coding-challenges/20-julia-set/sketch.js
@@ -1,57 +1,128 @@
 var canvas;
 let maxIterations = 100;
-let range = 5;
-let minSlider, maxSlider;
+let range;
+let oscillateACheck,oscillateBCheck,bwCheck;
 let currentPos;
+let angleA = 0;
+let angleB = 0;
+let imagesSaved = 0;
 
 function setup(){
-    canvas = createCanvas(360,360);
+    canvas = createCanvas(600,600);
     currentPos = createVector(0,0);
+    range = 1.4;
     pixelDensity(1);
     createP('');
-    createP('Min Slider');
-    minSlider = createSlider(-range, 0, -range, 0.01);
-    createP('Max Slider');
-    maxSlider = createSlider(0, range, range, 0.01);
+    bwCheck = createCheckbox('Black and White', false);
+    oscillateACheck = createCheckbox('Oscillate A', false);
+    oscillateBCheck = createCheckbox('Oscillate B', false);
+    oscillateACheck.changed(resetFocus);
+    oscillateBCheck.changed(resetFocus);
 }
 function draw(){
+    let w = 3;
+    let h = (w * height)/ width;
+    
+    let xmin = -w/2;
+    let ymin = -h/2;
+
     loadPixels();
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-            let a = map(x, 0, width, minSlider.value(), maxSlider.value());
-            let b = map(y, 0, height, minSlider.value(), maxSlider.value());
 
-            let ca = a;
-            let cb = b;
+    //set range for x
+    let xmax = xmin + w;
+    let ymax = ymin + h;
+    
+    //calculate change for each pixel
+    let dx = (xmax-xmin)/width;
+    let dy = (ymax-ymin)/height;
 
+    let ca,cb;
+    //julia set constants
+    if(oscillateACheck.checked() && oscillateBCheck.checked()){
+        ca = sin(angleA);
+        cb = cos(angleB);
+    } else if (oscillateACheck.checked()){
+        ca = sin(angleA);
+        cb = 0;
+    } else if(oscillateBCheck.checked()){
+        ca = 0;
+        cb = cos(angleB);
+    } else{
+        ca = map(mouseX, 0, width, -range, range);
+        cb = map(mouseY, 0, height, -range, range);
+    }
+
+    angleA += 0.03;
+    angleB += 0.03;
+    let a,b;
+
+    let y = ymin;
+    for (let j = 0; j < height; j++) {
+        let x = xmin;
+        for (let i = 0; i < width; i++) {
+            let a = x;
+            let b = y;
             let n = 0;
-            let z = 0;
 
             while(n < maxIterations){
-                let aa = a * a - b * b;
-                let bb = 2 * a * b;
+                let aa = a * a;
+                let bb = b * b;
+                let twoab = 2.0 * a * b;
 
-                a = aa + ca;
-                b = bb + cb;
+                a = aa - bb + ca;
+                b = twoab + cb;
 
-                if(abs(a + b) > 16){
-                // if((a*a+b*b)>4){
+                if(aa + bb > 4){
                     break;
                 }
-                n++
+                n++;
             }
 
-            let bright = map(n, 0, maxIterations, 0, 255);
-            if(n === maxIterations){
-                bright = 0;
+            let bright;
+            
+            //COLORS for gradient
+            //Blue
+            //RGB(0, 121, 191)
+            //Orange
+            //RGB(255, 165, 26)
+            let red = map(n, 0, maxIterations, 0, 255);
+            let green = map(n, 0, maxIterations, 121, 165);
+            let blue = map(n, 0, maxIterations, 191, 26);
+            
+            let pix = (j + i * width) * 4;
+            if(bwCheck.checked()){
+                bright = map(n, 0, maxIterations, 0, 255);
+                pixels[pix + 0] = bright; //red
+                pixels[pix + 1] = bright; //green
+                pixels[pix + 2] = bright; //blue
+                pixels[pix + 3] = 255;//alpha
+            } else{
+                bright = map(n, 0, maxIterations, 100, 255);
+                pixels[pix + 0] = red; //red
+                pixels[pix + 1] = green; //green
+                pixels[pix + 2] = blue; //blue
+                pixels[pix + 3] = bright;//alpha
             }
-
-            let pix = (x + y * width) * 4;
-            pixels[pix + 0] = bright; //red
-            pixels[pix + 1] = bright; //green
-            pixels[pix + 2] = bright; //blue
-            pixels[pix + 3] = 255;//alpha
+            x += dx;
         }
+        y += dy;
     }
     updatePixels();
+}
+
+function keyPressed(){
+    if(key === ' '){
+        let name = 'julia-set';
+        if(imagesSaved != 0){
+            name += '_';
+            save(name + imagesSaved + '.png');
+        } else{
+            save(name + '.png');
+        }
+        imagesSaved += 1;
+    }
+}
+
+function resetFocus(){
+    canvas.focus();
 }
---
+++ coding-challenges/21-perlin-flow-field/sketch.js
@@ -0,0 +1,32 @@
+let canvas;
+let noiseScale = 0.01;
+let scl = 20;
+let cols, rows;
+
+function setup(){
+    canvas = createCanvas(500,500);
+    background(40);
+    pixelDensity(1);
+}
+
+function draw(){
+    let yoff = 0;
+    loadPixels();
+    for (let y = 0; y < height; y++) {
+        let xoff = 0;
+        for (let x = 0; x < width; x++) {
+            let index = (x + y * width) * 4; //since is stores r,g,b,a for each pixel
+
+            let r = noise(xoff, yoff) * 255;
+            pixels[index + 0] = r;
+            pixels[index + 1] = r;
+            pixels[index + 2] = r;
+            pixels[index + 3] = 255;
+
+            xoff += noiseScale;
+        }
+        yoff += noiseScale;
+    }
+
+    updatePixels();
+}
