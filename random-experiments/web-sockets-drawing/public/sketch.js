e6a131caed12edd0ffc7f0b779380d818fb38cee
Samuel Mosley <samueljmosley@gmail.com>
2018-01-05 17:02:10 -0600

    finish the web-socket-drawing

--- random-experiments/web-sockets-drawing/public/sketch.js
+++ random-experiments/web-sockets-drawing/public/sketch.js
@@ -1,11 +1,42 @@
 let canvas;
+let socket;
+let col;
 
 function setup(){
     canvas = createCanvas(500,500);
+    socket = io.connect('http://localhost:3000');
+    socket.on('mouse', newDrawing);
+    col = {
+        r: random(255),
+        g: random(255),
+        b: random(255)
+    }
     background(40);
+    noStroke();
+}
+
+function newDrawing(data){
+    fill(data.r,data.g,data.b);
+    ellipse(data.x, data.y, 10,10);
 }
 
 function draw(){
     // background(40);
-    ellipse(mouseX, mouseY, 60,60);
+}
+
+function mouseDragged(){
+    if(mouseIsPressed){
+        let data = {
+            x: mouseX,
+            y: mouseY,
+            r:col.r,
+            g:col.g,
+            b:col.b
+        }
+
+        socket.emit('mouse', data);
+        // console.log(`Sending ${mouseX}, ${mouseY}`);
+        fill(255);
+        ellipse(mouseX, mouseY, 10,10);
+    }
 }
--- random-experiments/web-sockets-drawing/server.js
+++ random-experiments/web-sockets-drawing/server.js
@@ -2,8 +2,30 @@ let express = require('express');
 let app = express();
 let server = app.listen(3000);
 
+console.log('My socket server is running');
+
 app.use(express.static('public'));
 
 app.get('/', () => {
 
 })
+
+let socket = require('socket.io');
+let io = socket(server);
+
+io.sockets.on('connection',newConnection);
+
+function newConnection(socket){
+    // console.log('new connection: ' + socket.id);
+
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data){
+        //send to all other sockets
+        socket.broadcast.emit('mouse', data);
+        
+        //send to all sockets including self
+        //io.sockets.emit('mouse',data);
+        // console.log(data);
+    }
+}
